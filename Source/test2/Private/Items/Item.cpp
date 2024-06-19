3dc91223a9c913b635bd9ac263c98df32bb53048
ltw214171 <con2220@naver.com>
2024-06-19 18:19:22 +0900

    6장 + 던전 에러 수정

--- Source/test2/Private/Items/Item.cpp
+++ Source/test2/Private/Items/Item.cpp
@@ -4,13 +4,30 @@
 #include "Items/Item.h"
 #include "test2/DebugMacro.h"
 
+//Class Default Object (CDO) : 언리얼 엔진 리플렉션 시스템에 의해 생성되는 객체의 복사본,
+// 엔진이 시작될 때나 컴파일 시 생성됨, 리플렉션 시스템이 우리 클래스에 기반한 블루프린트 객체들을 초기화할 때 필요한 값들을 보관하기 위해 생성됨
+// 그래서 객체의 생성자에서 함수 등을 쓸 때 엔진의 모든 기능이 활성화되진 않았을 수 있으므로 주의해야 함
+// 따라서 생성자가 아닌 BeginPlay 등에서 생성 시 처리할 것들을 실행해야 함
+
+//컴포넌트에도 비슷한 현상이 일어남. 컴포넌트를 생성하기 위해선 디폴트 하위객체를 만들어야 함
+// 디폴트 하위객체는 CDO와 비슷하지만 컴포넌트의 디폴트 정보를 가지고 있음
+// 하위객체를 만들 때는 몇 가지 정보를 전달해야 함
+// 
+//		1. 객체 타입(UStaticMeshComponent 등)
+//		2. 내부 이름(컴포넌트 변수 이름과 다름)
+//		CreateDefaultSubobject<Type>(Name);
+//		
+//		이 함수는 새로 생성된 객체를 가리키는 포인터를 반환한다. 이런 함수를 팩토리라고 한다.
+//		언리얼 엔진에서는 팩토리 함수를 많이 사용하기 때문에 new를 직접 사용하지 않는다
 
 // Sets default values
-AItem::AItem()
+AItem::AItem() : RunningTime(0.f), Amplitude(0.25f), TimeConstant(5.f)
 {
 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	ItemMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("ItemMeshComponent"));	//디폴트 하위객체 생성 팩토리 함수
+	RootComponent = ItemMesh;
 }
 
 // Called when the game starts or when spawned
@@ -45,11 +62,26 @@ void AItem::BeginPlay()
 	//	DrawDebugPoint(World, Location + Forward * 100.f, 15.f, FColor::Red, true);
 	//}
 
-	DRAW_SPHERE(Location);
+	//DRAW_SPHERE(Location);
 	//DRAW_LINE(Location, Location + Forward * 100.f);
 	//DRAW_POINT(Location + Forward * 100.f);
-	DRAW_VECTOR(Location, Location + Forward * 100.f);
+	//DRAW_VECTOR(Location, Location + Forward * 100.f);
 
+	//int32 AvgInt = Avg<int32>(1, 3);
+	//UE_LOG(LogTemp, Warning, TEXT("Avg of 1 and 3: %d"), AvgInt);
+
+	//float AvgFloat = Avg<float>(3.45f, 7.86f);
+	//UE_LOG(LogTemp, Warning, TEXT("Avg of 3.45 and 7.86: %f"), AvgFloat);
+}
+
+float AItem::TransformedSin(float Value)
+{
+	return Amplitude * FMath::Sin(Value * TimeConstant);
+}
+
+float AItem::TransformedCos(float Value)
+{
+	return Amplitude * FMath::Cos(Value * TimeConstant);
 }
 
 // Called every frame
@@ -67,5 +99,22 @@ void AItem::Tick(float DeltaTime)
 
 		UE_LOG(LogTemp, Warning, TEXT("Item Name: %s"), *Name);
 	}
+
+	RunningTime += DeltaTime;
+	//float DeltaZ = Amplitude * FMath::Sin(RunningTime * TimeConstant);
+
+	//AddActorWorldOffset(FVector(0.f, 0.f, DeltaZ));
+
+	//FVector Location = GetActorLocation();
+	//DRAW_SPHERE_SINGLEFRAME(Location);
+	//DRAW_VECTOR_SINGLEFRAME(Location, Location + GetActorForwardVector() * 100.f);
+
+	//FVector AvgVector = Avg<FVector>(GetActorLocation(), FVector::ZeroVector);
+	//DRAW_POINT_SINGLEFRAME(AvgVector);
+
+	//FRotator AvgRotator = Avg<FRotator>(GetActorRotation(), FRotator::ZeroRotator);	//ERROR, FRotator에는 나누기 연산이 없음
+
+
+
 }
 
--- Source/test2/Public/Items/Item.h
+++ Source/test2/Public/Items/Item.h
@@ -19,8 +19,55 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Sine Parameter")	//BlueprintReadOnly : 블루프린트에 Getter 노드 생성
+																				//BlueprintReadWrite : 블루프린트에 Getter, Setter 노드 생성
+	float Amplitude;															//private 변수에서는 작동하지 않으므로 protected나 public이여야 함
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Sine Parameter")	//Category : 블루프린트 오른쪽의 Detail에 카테고리를 추가하거나 기존 카테고리에 삽입
+	float TimeConstant;
+
+	//UFUNCTION(BlueprintCallable);	//함수를 블루프린트에 노출하는 매크로
+									//BlueprintCallable : 함수를 블루프린트에 노출함
+	UFUNCTION(BlueprintPure)		//BlueprintPure : 함수를 블루프린트에 순수함수로 노출함
+	float TransformedSin(float Value);
+
+	UFUNCTION(BlueprintPure)
+	float TransformedCos(float Value);
+
+	template<typename T>
+	T Avg(T First, T Second);
+
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
+private:
+	//EditDefalutsOnly : 디폴트 블루프린트에서는 보고 수정할 수 있지만 월드에 있는 인스턴스에서는 불가능함
+	//EditInstanceOnly : 월드에 있는 인스턴스에서는 보고 수정할 수 있지만 디폴트 블루프린트에서는 불가능함
+	//EditAnywhere : 디폴트 블루프린트나 월드에 있는 인스턴스 둘 다에서 보거나 수정할 수 있음
+	//VisibleDefaultOnly : 디폴트 블루프린트에서 볼 수는 있지만 수정은 불가능함
+	//VisibleInstanceOnly : 월드에 있는 인스턴스에서는 볼 수 있지만 수정은 불가능함
+	//VisibleAnywhere : 디폴트 블루프린트나 월드에 있는 인스턴스 둘 다에서 볼 수 있지만 수정은 불가능함
+
+	//UPROPERTY(EditAnywhere);
+	//UPROPERTY(VisibleInstanceOnly);
+	//float RunningTime;
+
+	UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))	//<- private 변수도 블루프린트에 보이게 하는 문법
+	float RunningTime;
+
+	//UPROPERTY(EditAnywhere);
+	//float Amplitude;
+
+	//UPROPERTY(EditAnywhere);
+	//float TimeConstant;
+
+	UPROPERTY(VisibleAnywhere)
+	UStaticMeshComponent* ItemMesh;
 };
+
+template<typename T>
+inline T AItem::Avg(T First, T Second)
+{
+	return (First + Second) / 2;
+}
