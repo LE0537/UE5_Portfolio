6b8dc29f76189b82621f32f56bce370dd158c05f
ltw214171 <con2220@naver.com>
2024-06-21 03:21:07 +0900

    65강까지 진행, 맵 수정, 나무 흔들림 제거, LFS 수정 적용 확인

--- Source/test2/test2.Build.cs
+++ Source/test2/test2.Build.cs
@@ -12,12 +12,15 @@ public class test2 : ModuleRules
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
-		// Uncomment if you are using Slate UI
-		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
-		
-		// Uncomment if you are using online features
-		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
+        PrivateIncludePaths.Add("test2");
+        PublicIncludePaths.Add("test2");
 
-		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
-	}
+        // Uncomment if you are using Slate UI
+        // PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+
+        // Uncomment if you are using online features
+        // PrivateDependencyModuleNames.Add("OnlineSubsystem");
+
+        // To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+    }
 }
